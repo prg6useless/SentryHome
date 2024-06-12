0679f3815c342659dc14e47f3bad433a3f2a1e12
_shakya._.sarthak_ <shakya.sarthak002@gmail.com>
2024-06-12 20:52:04 +0545

    something

--- lib/pages/view_page.dart
+++ lib/pages/view_page.dart
@@ -1,3 +1,72 @@
+// import 'dart:async';
+// import 'dart:typed_data';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
+// class VideoFeedPage extends StatefulWidget {
+//   @override
+//   _VideoFeedPageState createState() => _VideoFeedPageState();
+// }
+
+// class _VideoFeedPageState extends State<VideoFeedPage> {
+//   late Uint8List _imageBytes;
+//   late Timer _timer;
+//   bool _isError = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _timer = Timer.periodic(Duration(milliseconds: 500), (_) {
+//       _fetchFrame();
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _timer.cancel();
+//     super.dispose();
+//   }
+
+//   Future<void> _fetchFrame() async {
+//     try {
+//       final response =
+//           await http.get(Uri.parse('http://192.168.1.69:8000/frame'));
+//       if (response.statusCode == 200) {
+//         setState(() {
+//           _imageBytes = response.bodyBytes;
+//           _isError = false;
+//         });
+//       } else {
+//         setState(() {
+//           _isError = true;
+//         });
+//       }
+//     } catch (e) {
+//       setState(() {
+//         _isError = true;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Video Stream'),
+//       ),
+//       body: Center(
+//         child: _isError
+//             ? Text(
+//                 'No Feed',
+//                 style: TextStyle(fontSize: 24.0),
+//               )
+//             : _imageBytes != null
+//                 ? Image.memory(_imageBytes) //stores the current frame coming from the sever
+//                 : CircularProgressIndicator(),
+//       ),
+//     );
+//   }
+// }
 import 'dart:async';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
@@ -9,15 +78,14 @@ class VideoFeedPage extends StatefulWidget {
 }
 
 class _VideoFeedPageState extends State<VideoFeedPage> {
-  late Uint8List _imageBytes;
+  Uint8List? _imageBytes;
   late Timer _timer;
-  bool _isError = false;
 
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(Duration(milliseconds: 500), (_) {
-      _fetchFrame();
+    _timer = Timer.periodic(Duration(milliseconds: 100), (_) {
+      _fetchFrame(); // Increase the frequency to 100 milliseconds
     });
   }
 
@@ -28,23 +96,15 @@ class _VideoFeedPageState extends State<VideoFeedPage> {
   }
 
   Future<void> _fetchFrame() async {
-    try {
-      final response =
-          await http.get(Uri.parse('http://192.168.101.3:8000/frame'));
-      if (response.statusCode == 200) {
-        setState(() {
-          _imageBytes = response.bodyBytes;
-          _isError = false;
-        });
-      } else {
-        setState(() {
-          _isError = true;
-        });
-      }
-    } catch (e) {
+    final response = await http.get(Uri.parse(
+        'http://192.168.1.80:8000/frame')); // Replace with your actual endpoint
+    if (response.statusCode == 200) {
       setState(() {
-        _isError = true;
+        _imageBytes = response.bodyBytes;
       });
+    } else {
+      // Handle error
+      print('Error fetching frame: ${response.statusCode}');
     }
   }
 
@@ -55,14 +115,9 @@ class _VideoFeedPageState extends State<VideoFeedPage> {
         title: Text('Video Stream'),
       ),
       body: Center(
-        child: _isError
-            ? Text(
-                'No Feed',
-                style: TextStyle(fontSize: 24.0),
-              )
-            : _imageBytes != null
-                ? Image.memory(_imageBytes)
-                : CircularProgressIndicator(),
+        child: _imageBytes != null
+            ? Image.memory(_imageBytes!) // Added null safety
+            : CircularProgressIndicator(),
       ),
     );
   }
